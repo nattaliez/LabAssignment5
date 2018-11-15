4f481c72e6638ca35dcdc400df701c27fb8b9f6d
Nattalie Zapien <nattaliez@mail.fresnostate.edu>
2018-11-15 09:54:41 -0800

    LabAssignment3

---
+++ src/app/main-header/main-header.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-header',
+  templateUrl: './main-header.component.html',
+  styleUrls: ['./main-header.component.css']
+})
+export class MainHeaderComponent implements OnInit {
+
+  private firstName: string;
+  private lastName: string;
+
+  constructor() {
+    this.firstName = 'Nattalie';
+    this.lastName = 'Zapien';
+  }
+
+
+  showGreeting() {
+    const banner = document.getElementById('banner');
+    banner.innerHTML = 'Hello' + this.firstName + ' ' + this.lastName + '.';
+  }
+
+  hideGreeting() {
+    const banner = document.getElementById('banner');
+    banner.innerHTML = 'r1c2';
+
+  }
+
+
+  ngOnInit() {
+  }
+
+}
